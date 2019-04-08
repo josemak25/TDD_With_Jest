613601fb6d9c2286282372c4c9da0f6ff6777e1b
josemak25 <amakirij@gmail.com>
2019-04-08 21:33:44 +0100

    add test for the totaal sum of inifnite function and error handling when param is not a number

--- __TEST__/jsJest.test.js
+++ __TEST__/jsJest.test.js
@@ -6,8 +6,17 @@ test("adds the sum of two integer numbers.", () => {
   expect(operations.add(5, 5)).toBe(10);
 });
 test("if add params is empty or not a number.", () => {
-  function throwError() {
-    operations.add("#", 12);
-  }
+  const throwError = () => operations.add("#", 12);
+  expect(throwError).toThrowError(Error);
+});
+
+//Checking for add function
+
+test("it should return the sum of all infinite numbers", () => {
+  expect(operations.addInfinite(1, 2, 3, 5)).toBe(11);
+});
+
+test("if infinite sum params is not a number. ", () => {
+  const throwError = () => operations.addInfinite(1, 2, 3, undefined);
   expect(throwError).toThrowError(Error);
 });
